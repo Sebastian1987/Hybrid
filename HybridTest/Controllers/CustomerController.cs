0e9c90b8458589e579368fb1baabc10b2418f509
Sebastian1987 <sebastian.csharp@gmail.com>
2017-02-02 09:14:11 -0500

    Full

---
+++ HybridTest/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+
+namespace HybridTest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        public static string ProcessPassword()
+        {
+            const int totalRolls = 25000;
+            int[] results = new int[6];
+
+            var passhash = "";            
+            for (int x = 0; x < totalRolls; x++)
+            {
+                byte roll = RollDice((byte)results.Length);
+                results[roll - 1]++;
+            }
+            for (int i = 0; i < results.Length; ++i)
+            {
+                passhash="{0}: {1} ({2:p1})" + i + 1 + results[i] + (double)results[i] / (double)totalRolls;
+            }
+            rngCsp.Dispose();
+
+            return passhash;
+        }
+
+               
+        public static byte RollDice(byte numberSides)
+        {
+            if (numberSides <= 0)
+                throw new ArgumentOutOfRangeException("numberSides");
+
+            byte[] randomNumber = new byte[1];
+            do
+            {             
+                rngCsp.GetBytes(randomNumber);
+            }
+            while (!IsFairRoll(randomNumber[0], numberSides));
+         
+            return (byte)((randomNumber[0] % numberSides) + 1);
+        }
+
+        private static bool IsFairRoll(byte roll, byte numSides)
+        {            
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+           
+            return roll < numSides * fullSetsOfValues;
+        }
+
+    }
+}
---
+++ HybridTest/ControllersApi/CustomerApiController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace HybridTest.ControllersApi
+{
+
+    public class CustomerApiController : ApiController
+    {    
+        [HttpGet]
+        public List<HybridTest.Models.CustomerModel> Customer(string id)
+        {
+            List<HybridTest.Models.CustomerModel> oList = new List<HybridTest.Models.CustomerModel>();
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                using (prodigiousEntities db = new prodigiousEntities())
+                {
+                    var Customer = db.Customer.ToList().Where
+                         (x => x.CustomerID == int.Parse(id)).Select(x => x).FirstOrDefault();
+
+                    if (Customer != null)
+                    {
+                        oList.Add(new Models.CustomerModel()
+                        {
+                            CustomerId = Customer.CustomerID,
+                            NameStyle = Customer.NameStyle,
+                            Title = Customer.Title,
+                            FirstName = Customer.FirstName,
+                            MiddleName = Customer.MiddleName,
+                            LastName = Customer.LastName,
+                            Suffix = Customer.Suffix,
+                            CompanyName = Customer.CompanyName,
+                            SalesPerson = Customer.SalesPerson,
+                            EmailAddress = Customer.EmailAddress,
+                            Phone = Customer.Phone,
+                            PasswordHash = Customer.PasswordHash,
+                            PasswordSalt = Customer.PasswordSalt,
+                            rowguid = Customer.rowguid,
+                            ModifiedDate = Customer.ModifiedDate,
+                        });
+                    }
+                    
+                }
+            }
+            else
+            {
+                using (prodigiousEntities db = new prodigiousEntities())
+                {
+
+                    db.Customer.ToList().All
+                        (x =>
+                        {
+                            oList.Add(new Models.CustomerModel()
+                            {
+                                CustomerId = x.CustomerID,
+                                NameStyle = x.NameStyle,
+                                Title = x.Title,
+                                FirstName = x.FirstName,
+                                MiddleName = x.MiddleName,
+                                LastName = x.LastName,
+                                Suffix = x.Suffix,
+                                CompanyName = x.CompanyName,
+                                SalesPerson = x.SalesPerson,
+                                EmailAddress = x.EmailAddress,
+                                Phone = x.Phone,
+                                PasswordHash = x.PasswordHash,
+                                PasswordSalt = x.PasswordSalt,
+                                rowguid = x.rowguid,
+                                ModifiedDate = x.ModifiedDate,
+                            });
+                            return true;
+                        });
+                }
+            }
+            return oList.OrderByDescending(x => x.ModifiedDate).ToList();
+        }
+
+        [HttpPost]
+        public void UpdateCustomer(string update)
+        {
+
+            List<HybridTest.Models.CustomerModel> oCustomer = new JavaScriptSerializer().Deserialize<List<HybridTest.Models.CustomerModel>>(System.Web.HttpContext.Current.Request["models"]);
+            using (prodigiousEntities db = new prodigiousEntities())
+            {
+                oCustomer.All(x =>
+            {
+                db.Entry(new HybridTest.Customer()
+                {
+                    CustomerID = x.CustomerId,
+                    NameStyle = x.NameStyle,
+                    Title = x.Title,
+                    FirstName = x.FirstName,
+                    MiddleName = x.MiddleName,
+                    LastName = x.LastName,
+                    Suffix = x.Suffix,
+                    CompanyName = x.CompanyName,
+                    SalesPerson = x.SalesPerson,
+                    EmailAddress = x.EmailAddress,
+                    Phone = x.Phone,
+                    PasswordHash = x.PasswordHash,
+                    PasswordSalt = x.PasswordSalt,
+                    rowguid = x.rowguid,
+                    ModifiedDate = x.ModifiedDate
+                }).State = EntityState.Modified;
+                return true;
+            });
+                db.SaveChanges();
+            }
+        }
+        [HttpPost]
+        public void CreateCustomer(string create)
+        {
+            try
+            {
+                List<HybridTest.Models.CustomerModel> oCustomer = new JavaScriptSerializer().Deserialize<List<HybridTest.Models.CustomerModel>>(System.Web.HttpContext.Current.Request["models"]);
+                using (prodigiousEntities db = new prodigiousEntities())
+                {
+                    var oToInsert = db.Set<Customer>();
+                    oCustomer.All(x =>
+                    {
+                        db.Entry(new HybridTest.Customer()
+                        {
+                            NameStyle = x.NameStyle,
+                            Title = x.Title,
+                            FirstName = x.FirstName,
+                            MiddleName = x.MiddleName,
+                            LastName = x.LastName,
+                            Suffix = x.Suffix,
+                            CompanyName = x.CompanyName,
+                            SalesPerson = x.SalesPerson,
+                            EmailAddress = x.EmailAddress,
+                            Phone = x.Phone,
+                            rowguid = Guid.NewGuid(),
+                            PasswordHash = HybridTest.Controllers.CustomerController.ProcessPassword(),
+                            PasswordSalt = HybridTest.Controllers.CustomerController.ProcessPassword(),
+                            ModifiedDate = DateTime.Now
+                        }).State = EntityState.Added;
+                        return true;
+                    });
+
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        [HttpPost]
+        public void DeleteCustomer(string Delete)
+        {
+            List<HybridTest.Models.CustomerModel> oCustomer = new JavaScriptSerializer().Deserialize<List<HybridTest.Models.CustomerModel>>(System.Web.HttpContext.Current.Request["models"]);
+            using (prodigiousEntities db = new prodigiousEntities())
+            {
+                oCustomer.All(x =>
+                {
+                    db.Entry(new HybridTest.Customer()
+                    {
+                        CustomerID = x.CustomerId,
+                        NameStyle = x.NameStyle,
+                        Title = x.Title,
+                        FirstName = x.FirstName,
+                        MiddleName = x.MiddleName,
+                        LastName = x.LastName,
+                        Suffix = x.Suffix,
+                        CompanyName = x.CompanyName,
+                        SalesPerson = x.SalesPerson,
+                        EmailAddress = x.EmailAddress,
+                        Phone = x.Phone,
+                        PasswordHash = x.PasswordHash,
+                        PasswordSalt = x.PasswordSalt,
+                        rowguid = x.rowguid,
+                        ModifiedDate = x.ModifiedDate
+                    }).State = EntityState.Deleted;
+                    return true;
+                });
+
+                db.SaveChanges();
+            }
+        }
+    }
+
+}
