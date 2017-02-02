d837c2e1ca7bb77c8d72e68b0058f445f48e2bf6
Sebastian1987 <sebastian.csharp@gmail.com>
2017-02-02 09:55:18 -0500

    Final Project

--- HybridTest/Controllers/CustomerController.cs
+++ HybridTest/Controllers/CustomerController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Security.Cryptography;
 
@@ -15,13 +12,12 @@ namespace HybridTest.Controllers
             return View();
         }
 
-        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
         public static string ProcessPassword()
         {
             const int totalRolls = 25000;
             int[] results = new int[6];
 
-            var passhash = "";            
+            var passhash = "";
             for (int x = 0; x < totalRolls; x++)
             {
                 byte roll = RollDice((byte)results.Length);
@@ -29,14 +25,22 @@ namespace HybridTest.Controllers
             }
             for (int i = 0; i < results.Length; ++i)
             {
-                passhash="{0}: {1} ({2:p1})" + i + 1 + results[i] + (double)results[i] / (double)totalRolls;
+                passhash = "{0}: {1} ({2:p1})" + i + 1 + results[i] + (double)results[i] / (double)totalRolls;
             }
             rngCsp.Dispose();
 
             return passhash;
         }
 
-               
+        #region Privated Functions
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();        
+
+        private static bool IsFairRoll(byte roll, byte numSides)
+        {
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+
+            return roll < numSides * fullSetsOfValues;
+        }
         public static byte RollDice(byte numberSides)
         {
             if (numberSides <= 0)
@@ -44,20 +48,13 @@ namespace HybridTest.Controllers
 
             byte[] randomNumber = new byte[1];
             do
-            {             
+            {
                 rngCsp.GetBytes(randomNumber);
             }
             while (!IsFairRoll(randomNumber[0], numberSides));
-         
+
             return (byte)((randomNumber[0] % numberSides) + 1);
-        }
-
-        private static bool IsFairRoll(byte roll, byte numSides)
-        {            
-            int fullSetsOfValues = Byte.MaxValue / numSides;
-           
-            return roll < numSides * fullSetsOfValues;
-        }
-
+        } 
+        #endregion
     }
 }
